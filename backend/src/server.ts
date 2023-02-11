64f5dd7b67a9b5bb14fa313f9f4c412ecea6c643
Vianney <vianney.touillon@epitech.eu>
2023-02-11 20:10:06 +0100

    {fix} prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,15 @@ import express from 'express';
 import * as Express from 'express';
 import morgan from 'morgan';
 import serveFavicon from 'serve-favicon';
+import { PrismaClient } from "@prisma/client"
+import prisma from './prisma';
 
 const server = express();
 const port = 8080;
+
+server.get('/Beer', (req, res) => {
+    res.send(prisma.beer); 
+});
 
 server.listen(port, () => {
     console.log('Server listening on http://localhost:8080/')
