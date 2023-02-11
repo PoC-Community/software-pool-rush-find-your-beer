98a5b8be4207638c1697c509f4bbbcfc8177d20e
Matteo Duprat <matteo.duprat@epitech.eu>
2023-02-11 21:29:44 +0100

    route

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,23 +1,23 @@
-import { Center, Spinner, Text, VStack } from '@chakra-ui/react';
-import Home from '../pages/Home';
-import Question1 from '../pages/Question1'
-import React from 'react'
-import {ChakraProvider} from "@chakra-ui/react";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { Center, Spinner, Text, VStack } from "@chakra-ui/react";
+import Home from "../pages/Home";
+import Question1 from "../pages/Question1";
+import Question2 from "../pages/Question2";
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes, BrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Home />}>
-      <Route path="question1" element={<Question1 />} />
-    </Route>
-  )
-);
-
-function App () {
-  return(
-<ChakraProvider>
-    <RouterProvider router={router}></RouterProvider>
-</ChakraProvider>
-  )
+function App() {
+  return (
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="question1" element={<Question1 />} />
+          <Route path="question2" element={<Question2 />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
 }
+
 export default App;
--- frontend/src/pages/Question1.tsx
+++ frontend/src/pages/Question1.tsx
@@ -1,12 +1,82 @@
-import { Center, Text, VStack, Heading, Box , Button, ButtonGroup, Image } from '@chakra-ui/react';
-import React, {useState, useEffect} from 'react';
+import {
+  Center,
+  Text,
+  VStack,
+  RangeSlider,
+  RangeSliderTrack,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  Button, Heading
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import React from "react";
+
+function Rslider () {
+  return(
+    <RangeSlider aria-label={['min', 'max']} defaultValue={[1, 1000]}>
+      <RangeSliderTrack>
+        <RangeSliderFilledTrack />
+      </RangeSliderTrack>
+      <RangeSliderThumb index={0} />
+      <RangeSliderThumb index={1} />
+    </RangeSlider>
+  )
+}
+
+function Nslider () {
+  return(
+    <Slider aria-label='slider-ex-1' defaultValue={30}>
+      <SliderTrack>
+        <SliderFilledTrack />
+      </SliderTrack>
+      <SliderThumb />
+    </Slider>
+  )
+}
+
+function MyButton() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/question2");
+  };
+  return (
+    <Center mt="0px">
+      <VStack spacing={4} direction='row' align='center'>
+        <Button colorScheme='messenger' size='lg' onClick={handleClick}>
+          NEXT
+        </Button>
+      </VStack>
+    </Center>
+  );
+}
 
 const Question1 = (): JSX.Element => (
-  <div style={{ backgroundColor: "white", height: "20vh" }}>
-    <Center mt="160px">
+  <div>
+    <Center mt="64px">
+      <Heading size='4xl' fontSize='100px'>
+        Stats
+      </Heading>
+    </Center>
+    <Center mt="64px">
+      <Text fontSize='2xl'>0</Text>
       <VStack spacing="32px">
-        <Text fontSize='md'>QUESTION 1</Text>
+        <Text fontSize='4xl'>IBU(International Bitterness Units)</Text>
+        <Rslider/>
       </VStack>
+      <Text fontSize='2xl'>100</Text>
+    </Center>
+    <Center mt="64px">
+      <Text fontSize='2xl'>0</Text>
+      <VStack spacing="32px">
+        <Text fontSize='4xl'>ABV(Alcool By Volume)</Text>
+        <Nslider/>
+        <MyButton/>
+      </VStack>
+      <Text fontSize='2xl'>57.5</Text>
     </Center>
   </div>
 );
